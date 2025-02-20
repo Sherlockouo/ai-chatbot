7619c93240a01153444471333c4805f59ec3d1ca
吴顶峰 <wudingfeng@msh.team>
2025-02-20 19:16:42 +0800

    fix: fix scroll message

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,19 +1,19 @@
-import { cookies } from 'next/headers';
+import { cookies } from "next/headers";
 
-import { Chat } from '@/components/chat';
-import { DEFAULT_CHAT_MODEL } from '@/lib/ai/models';
-import { generateUUID } from '@/lib/utils';
-import { DataStreamHandler } from '@/components/data-stream-handler';
+import { Chat } from "@/components/chat";
+import { DEFAULT_CHAT_MODEL } from "@/lib/ai/models";
+import { generateUUID } from "@/lib/utils";
+import { DataStreamHandler } from "@/components/data-stream-handler";
 
 export default async function Page() {
   const id = generateUUID();
 
   const cookieStore = await cookies();
-  const modelIdFromCookie = cookieStore.get('chat-model');
+  const modelIdFromCookie = cookieStore.get("chat-model");
 
   if (!modelIdFromCookie) {
     return (
-      <>
+      <div className="flex w-full">
         <Chat
           key={id}
           id={id}
@@ -23,12 +23,12 @@ export default async function Page() {
           isReadonly={false}
         />
         <DataStreamHandler id={id} />
-      </>
+      </div>
     );
   }
 
   return (
-    <>
+    <div className="flex w-full">
       <Chat
         key={id}
         id={id}
@@ -38,6 +38,6 @@ export default async function Page() {
         isReadonly={false}
       />
       <DataStreamHandler id={id} />
-    </>
+    </div>
   );
 }
